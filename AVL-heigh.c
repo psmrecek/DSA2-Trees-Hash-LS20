6e23d1a811fa6c553fad1409debe785cb9a126b8
Peter Smrecek <petersmrecek@gmail.com>
2020-04-04 21:35:39 +0200

    Randomizer pre AVL

--- AVL-heigh.c
+++ AVL-heigh.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Peter Smrecek
 
@@ -17,30 +19,30 @@ struct Node
 //}
 //
 //int heightAVLrec(struct Node* N) {
-//	if (N == NULL)						// Ak neexistuje, vrati 0
+//	if (N == NULL)												// Ak neexistuje, vrati 0
 //		return 0;
 //
-//	int left = heightAVLrec(N->L);		// Rekurzivne vyrata vysku laveho podstromu
-//	int right = heightAVLrec(N->R);		// Rekurzivne vyrata vysku praveho podstromu
+//	int left = heightAVLrec(N->L);								// Rekurzivne vyrata vysku laveho podstromu
+//	int right = heightAVLrec(N->R);								// Rekurzivne vyrata vysku praveho podstromu
 //
-//	return max(left, right) + 1;		// Vrati vacsie cislo a prirata 1
+//	return max(left, right) + 1;								// Vrati vacsie cislo a prirata 1
 //}
 //
-//int balanceFactorAVLrec(struct Node* N) {
+//int balanceFactorAVLrec(struct Node* N) {						// Rata balance factor cez rekurzivnu funkciu
 //	return heightAVLrec(N->L) - heightAVLrec(N->R);
 //}
 
-int height(struct Node* N) {
+int height(struct Node* N) {									// Getter na vysku uzla
 	if (N != NULL)
 		return N->height;
-	return -1;
+	return -1;													// Neexistujuci uzol ma vysku -1
 }
 
-void setHeight(struct Node** N) {
+void setHeight(struct Node** N) {								// Setter pre vysku uzla, nastavi vacsiu z vysok podstromov
 	(*N)->height = height((*N)->L) > height((*N)->R) ? height((*N)->L) + 1 : height((*N)->R) + 1;
 }
 
-struct Node* rightRotAVL(struct Node* N) {
+struct Node* rightRotAVL(struct Node* N) {						// Rotacia doprava
 	// LL
 	struct Node* newN = N->L;
 	N->L = newN->R;
@@ -94,17 +96,13 @@ struct Node* insertAVL(struct Node* N, int key) {
 	if (N->key == key)
 		return N;
 
-	if (key < N->key) {
+	if (key < N->key)
 		N->L = insertAVL(N->L, key);
-	}
-	else{
+	else
 		N->R = insertAVL(N->R, key);
-	}
-
 
 	setHeight(&N);
 	int balance = height(N->L) - height(N->R);
-
 
 	if (balance == 2)						// LL a LR 
 	{
@@ -121,6 +119,31 @@ struct Node* insertAVL(struct Node* N, int key) {
 		else
 			return rightLeftRotAVL(N);		// RL
 	}
+
+
+	//// Left Left Case 
+	//if (balance > 1 && key < N->L->key)
+	//	return rightRotAVL(N);
+
+	//// Right Right Case 
+	//if (balance < -1 && key > N->R->key)
+	//	return leftRotAVL(N);
+
+	//// Left Right Case 
+	//if (balance > 1 && key > N->L->key)
+	//{
+	//	N->L = leftRotAVL(N->L);
+	//	return rightRotAVL(N);
+	//}
+
+	//// Right Left Case 
+	//if (balance < -1 && key < N->R->key)
+	//{
+	//	N->R = rightRotAVL(N->R);
+	//	return leftRotAVL(N);
+	//}
+
+
 	return N;
 }
 
@@ -136,8 +159,8 @@ void preOrderAVL(struct Node* N) {		// Preorder vypis (podla prezentacie)
 void preOrderAVL2(struct Node* N) {		// Preorder vypis (podla prezentacie)
 	if (N != NULL)
 	{
-		printf("Kluc %d ma balans %d a ma vysku %d\n", N->key, balanceFactorAVLrec(N), N->height);
-
+//		printf("Kluc %d ma balans %d a ma vysku %d\n", N->key, balanceFactorAVLrec(N), N->height);
+		printf("Kluc %d ma vysku %d\n", N->key, N->height);
 		preOrderAVL2(N->L);
 		preOrderAVL2(N->R);
 	}
@@ -162,37 +185,34 @@ void leftAVL(struct Node* N) {
 int main() {
 	struct Node* root = NULL;
 
-	root = insertAVL(root, 10);
-	root = insertAVL(root, 20);
-	root = insertAVL(root, 25);
-	root = insertAVL(root, 30);
-	root = insertAVL(root, 40);
-	root = insertAVL(root, 50);
-	root = insertAVL(root, 25);
-//	root = insertAVL(root, 5);
-	printf("---------------\n");
+	int seed = 11;
+	int min = 1;
+	int max = 11;
+	int lenght = 10;
+
+	srand(seed);
+	max++;
+	for (int i = 0; i < lenght; i++)
+	{
+		int k = (rand() % (max - min)) + min;
+		printf("%d ", k);
+		root = insertAVL(root, k);
+	}
+
+	//root = insertAVL(root, 10);
+	//root = insertAVL(root, 20);
+	//root = insertAVL(root, 25);
+	//root = insertAVL(root, 30);
+	//root = insertAVL(root, 40);
+	//root = insertAVL(root, 50);
+	//root = insertAVL(root, 25);
+
+	printf("\n---------------\n");
 	printf("\nPreorder2\n");
 	preOrderAVL2(root);
 
-
-
-	//	
-	//	
-	//	
-	////	
-	////	root = insertAVL(root, 35);
-	//
-	//	printf("\nPreorder2\n");
-	//	preOrderAVL2(root);
-	//
 	printf("\nPreorder\n");
 	preOrderAVL(root);
-
-	//printf("\nRight only\n");
-	//rightAVL(root);
-
-	//printf("\nLeft only\n");
-	//leftAVL(root);
 
 	return 0;
 }
